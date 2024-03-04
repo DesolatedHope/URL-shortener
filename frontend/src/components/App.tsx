c28ab521110e7b5d3c85d1e7d18068b81e31f082
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-04 12:35:33 +0530

    Updated Login and Signup pages

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,16 +1,22 @@
 // import { Home } from './components/Home/Home'
-import React from 'react';
+import { ReactDOM } from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import { Navbar, Home } from ".";
+import SignUp from './SignUp/SignUp';
+import Login from "./LogIn/LogIn";
 // import cubes from "./assets/Cubes.svg";
 // import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   // const kalidas = true;
   return (
-    <div className="absolute h-full w-full bg-elite-black -z-40">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes >
+        <Route index element={<Home />} />
+        <Route path="LogIn" element={<Login />} /> 
+        <Route path="SignUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,15 +1,16 @@
 import swirl from "../../assets/Swirl.svg";
-import { WelcomePage } from "../index";
+import { Navbar, WelcomePage } from "../index";
 import { TanStackTable } from '../index';
 const Home = () => {
   const auth = true;
   if (auth) {
     return (
       <>
-        <img
+        <Navbar />
+        {/* <img
           src={swirl}
           className="-z-50 absolute top-0 left-0 h-full w-full"
-        />
+        /> */}
         {/* <img src={cubes} className="-z-20 absolute top-0 left-0" /> */}
         <div className="pt-4 min-h-screen bg-gray-900">
           <TanStackTable />
@@ -19,10 +20,11 @@ const Home = () => {
   } else {
     return (
       <>
-        <img
+        <Navbar />
+        {/* <img
           src={swirl}
           className="-z-50 absolute top-0 left-0 h-full w-full"
-        />
+        /> */}
         {/* <img src={cubes} className="-z-20 absolute top-0 left-0" /> */}
         <WelcomePage />
       </>
