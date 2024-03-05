fa086cb7aed579f0fcb66e7adc275ab093509fd5
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2024-03-05 23:24:55 +0530

    Revert "Changed the location of input on dashboard"

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -5,15 +5,34 @@ import Features from "../Features/Features";
 import Pricing from "../Pricing/Pricing";
 import { Navbar, WelcomePage } from "../index";
 import { TanStackTable } from '../index';
-import Input from './Input.tsx'
 const Home = () => {
+
+  const [longURL,setLongURL] = useState("");
+  const [shortURL,setShortURL] = useState("");
+
+  const handleSubmit = () => {
+    console.log("hello")
+    console.log(longURL);
+    const getShortURL = async () => {
+      const response = await instance.post('/api/getShortURL', {
+        longURL: longURL
+      },
+      {
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+      )
+      setShortURL(response.data.shortURL);
+    }
+    getShortURL();
+  }
 
   const auth = true;
   if (auth) {
     return (
       <>
         <Navbar />
-        <Input />
         {/* <img
           src={swirl}
           className="-z-50 absolute top-0 left-0 h-full w-full"
