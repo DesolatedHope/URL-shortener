d0e1e656c2066d31f2e159ee3356c99e4efa5ad5
Shashvat <shashvatpatel2803@gmail.com>
2024-03-06 01:33:18 +0530

    Added SVG and fixed log in sign up & welcome page

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,31 @@
 import React from "react";
 import logo from "../../assets/Logo.svg";
-import { useStateValue } from "../../MyContexts/StateProvider.jsx";
+import { useStateValue } from "../../MyContexts/StateProvider";
 
 const Navbar = () => {
-
   const [{ token }, dispatch] = useStateValue();
-  
+
   const handleClick = () => {
     dispatch({
       type: "SET_TOKEN",
       token: null,
     });
-  }
+  };
 
   if (token) {
     return (
-      <div className="sticky top-0 z-50 p-4 flex items-center justify-between bg-elite-black">
+      <div className="sticky top-0 z-50 p-4 flex items-center justify-between backdrop-blur-md backdrop-filter">
         {/* Logo on the left side */}
         <div className="flex items-center">
-          <img src={logo} alt="Logo" className="mr-2" />
+          <img src={logo} alt="Logo" className="mr-2 h-[100px] w-[150px]" />
         </div>
-
-        {/* Input Component in the center */}
-        {/* <Input /> */}
 
         {/* Buttons on the right side */}
         <div className="flex items-center">
-          <button onClick={()=>handleClick()} className="bg-bright-blue h-[50px] w-[120px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
+          <button
+            onClick={() => handleClick()}
+            className="bg-bright-blue h-[50px] w-[120px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2"
+          >
             Signout
           </button>
           <button className="bg-bright-pink text-white p-2 rounded-full">
@@ -51,9 +50,9 @@ const Navbar = () => {
   } else {
     return (
       <div className="w-full bg-transparent z-50 flex py-6 bg-[#0B101B] justify-between items-center navbar">
-        <p className="ml-10 text-[40px] text-transparent bg-gradient-to-r from-bright-pink to-bright-blue bg-clip-text font-bold justify-center z-50 center">
-          Shorty-URL
-        </p>
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="mr-2 h [100px] w-[150px]" />
+        </div>
         <ul className="list-none sm:flex hidden justify-end items-center flex-1">
           <li className="font-poppins font-bold cursor-pointer text-[16px] text-white mr-5">
             <button className="bg-dark-gray font-bold hover:bg-bright-blue text-lite-gray h-[50px] w-[125px] hover:text-white py-2 px-8 border-3 border-elite-black hover:border-transparent rounded-full">
