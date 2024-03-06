5aeef50349f7e90b4e255799d7cb674bb57f90d2
Himdyuti1 <himdyutisandilya@kgpian.iitkgp.ac.in>
2024-03-06 14:23:09 +0530

    Handled analytics

--- frontend/src/components/LogIn/LogIn.tsx
+++ frontend/src/components/LogIn/LogIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../assets/Logo.svg';
 import { useState } from 'react'
 import instance from '../../axios';
